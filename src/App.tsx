c0c69fb93f5b047182e74a5eea3318c90ee09c4e
pomirko <83339057+pomirko@users.noreply.github.com>
2024-07-17 20:05:55 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,208 @@
+import { useMemo, useState } from 'react';
+import { useMount, useUpdateEffect } from 'react-use';
+
+import axios, { AxiosResponse } from 'axios';
+import { Table, Image, Select, Typography, Space } from 'antd';
+import type { TableProps } from 'antd';
+
+import './App.css';
+
+enum Currencies {
+  usd = 'usd',
+  eur = 'eur',
+}
+
+enum Orders {
+  market_cap_desc = 'market_cap_desc',
+  market_cap_asc = 'market_cap_asc',
+}
+
+enum FilterTypes {
+  currecny = 'currency',
+  marketCap = 'marketCap',
+}
+
+type CurrencyProps = {
+  id: string;
+  name: string;
+  current_price: string;
+  circulating_supply: string;
+  image: string;
+};
+
+type DataType = CurrencyProps[];
+
+type FiltersProps = {
+  vs_currency: Currencies;
+  order: Orders;
+  per_page: number;
+  page: number;
+};
+
+type SelectFilterOptionsProps = { value: string; label: string };
+
+type SelectFilterProps<T> = {
+  defaultValue: T;
+  handleChange: (value: T, type: FilterTypes) => void;
+  options: SelectFilterOptionsProps[];
+  type: FilterTypes;
+};
+
+const curreciesOptions = [
+  { value: Currencies.usd, label: 'USD' },
+  { value: Currencies.eur, label: 'EUR' },
+];
+
+const marketCapOptions = [
+  { value: Orders.market_cap_desc, label: 'Market cap descending' },
+  { value: Orders.market_cap_asc, label: 'Market cap ascending' },
+];
+
+const { Title } = Typography;
+
+const fetchDataFn = (
+  params: FiltersProps
+): Promise<AxiosResponse<DataType>> => {
+  return axios.get(
+    'https://api.coingecko.com/api/v3/coins/markets?sparkline=false',
+    { params }
+  );
+};
+
+const SelectFilterWrapper = <T extends Orders | Currencies>({
+  defaultValue,
+  handleChange,
+  options,
+  type,
+}: SelectFilterProps<T>) => (
+  <Select
+    defaultValue={defaultValue}
+    style={{ width: 'fit-content', minWidth: 240 }}
+    onChange={(value) => handleChange(value, type)}
+    options={options}
+  />
+);
+
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<DataType>([]);
+  const [filters, setFilters] = useState<FiltersProps>({
+    vs_currency: Currencies.usd,
+    order: Orders.market_cap_desc,
+    per_page: 10,
+    page: 1,
+  });
+
+  const tableColumns: TableProps<CurrencyProps>['columns'] = useMemo(
+    () => [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (_, coin: CurrencyProps) => (
+          <div>
+            <Image width={32} height={32} src={coin.image} /> {coin.name}
+          </div>
+        ),
+      },
+      {
+        title: 'Current Price',
+        dataIndex: 'current_price',
+        key: 'current_price',
+        render: (text) => (
+          <span>
+            {text} {filters.vs_currency}
+          </span>
+        ),
+      },
+      {
+        title: 'Circulating Supply',
+        dataIndex: 'circulating_supply',
+        key: 'circulating_supply',
+      },
+    ],
+    [filters.vs_currency]
+  );
+
+  const getData = async () => {
+    await fetchDataFn(filters)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert('Error, try again');
+        console.error(error);
+        setIsLoading(false);
+      });
+  };
+
+  const handleChangeFilters = <T extends Orders | Currencies>(
+    value: T,
+    type: FilterTypes
+  ) => {
+    if (type === FilterTypes.currecny) {
+      setFilters((state) => ({ ...state, vs_currency: value as Currencies }));
+    }
+    if (type === FilterTypes.marketCap) {
+      setFilters((state) => ({ ...state, order: value as Orders }));
+    }
+  };
+
+  const handleChangePagination = (page: number) =>
+    setFilters((state) => ({ ...state, page }));
+
+  const handlePageSizeChange = (currentPage: number, pageSize: number) =>
+    setFilters((state) => ({
+      ...state,
+      page: currentPage,
+      per_page: pageSize,
+    }));
+
+  useMount(() => {
+    setIsLoading(true);
+    getData();
+  });
+
+  useUpdateEffect(() => {
+    setIsLoading(true);
+    getData();
+  }, [filters]);
+
+  return (
+    <>
+      <Title level={2}>Coins & Markets</Title>
+      <Space size="middle" style={{ margin: '12px 0' }}>
+        <SelectFilterWrapper
+          defaultValue={Currencies.usd}
+          handleChange={handleChangeFilters}
+          options={curreciesOptions}
+          type={FilterTypes.currecny}
+        />
+        <SelectFilterWrapper
+          defaultValue={Orders.market_cap_desc}
+          handleChange={handleChangeFilters}
+          options={marketCapOptions}
+          type={FilterTypes.marketCap}
+        />
+      </Space>
+      <Table
+        loading={isLoading}
+        columns={tableColumns}
+        dataSource={data}
+        rowKey="id"
+        pagination={{
+          total: 100000,
+          current: filters.page,
+          pageSize: filters.per_page,
+          showSizeChanger: true,
+          pageSizeOptions: ['5', '10', '20', '50', '100'],
+          onChange: handleChangePagination,
+          onShowSizeChange: handlePageSizeChange,
+        }}
+      />
+    </>
+  );
+};
+
+export default App;
